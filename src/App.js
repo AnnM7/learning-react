487e47ab9197705978c0fe9944a6cf84dc769fd1
AnnM7 <annam210399@yandex.ru>
2022-06-09 18:28:35 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-import TodoList from "./Todo/TodoList";
-import Context from "./context";
+import React, {useEffect} from "react"
+import TodoList from "./Todo/TodoList"
+import Context from "./context"
+import AddTodo from "./Todo/AddTodo"
+import Loader from "./loader";
+import Modal from "./Modal/Modal"
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {id:1, completed: false, title: 'Купить хлеб'},
-        {id:2, completed: false, title: 'Купить масло'},
-        {id:3, completed: false, title: 'Купить молоко'},
-    ])
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
 
-   function toggleTodo(id){
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() =>{
+                    setTodos(todos)
+                    setLoading(false)
+                }, 2000)
+            })
+    }, [])
+
+    function toggleTodo(id){
         setTodos(
         todos.map(todo=> {
             if(todo.id === id){
@@ -18,25 +29,37 @@ function App() {
             return todo
         })
         )
-   }
+    }
 
     function removeTodo(id){
         setTodos(todos.filter(todo => todo.id !== id))
     }
 
-  return (
+    function addTodo(title) {
+        setTodos(todos.concat([{
+            title,
+            id: Date.now(),
+            completed: false
+        }]))
+    }
+
+    return (
       <Context.Provider value={{removeTodo: removeTodo}}>
       <div className='wrapper'>
         <h1>React tutorial</h1>
+          <Modal />
 
+          <AddTodo onCreate={addTodo} />
+
+          {loading && <Loader />}
           {todos.length ? (
               <TodoList todos={todos} onToggle={toggleTodo} />
             ) : (
-                <p>No todos!</p>
+                loading ? null : <p>No todos!</p>
           )}
       </div>
       </Context.Provider>
-  )
-}
+    )
+    }
 
 export default App;
